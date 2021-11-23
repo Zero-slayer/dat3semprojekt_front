157f84b421890f2a07f4d5098d488fbfff304f76
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-11-23 14:23:23 +0100

    Updated with bootstrap

---
+++ src/FrontPage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './FrontPage.css';
+export default function FrontPage() {
+
+    return (
+        <div>
+            <h1 id="header">Worldwide COVID-19 cases</h1>
+
+            <form>
+                <div class="container" id="box1">
+                    <div class="mb-3">
+                        <label for="countryTextInput" class="form-label">Country: </label>
+                        <select id="countryTextInput" class="form-select">
+                            <option>Country</option>
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="filterTextInput" class="form-label">Filter:</label>
+                        <select id="filterTextInput" class="form-select">
+                            <option selected>Filter</option>
+                            <option value="1">Confirmed cases</option>
+                            <option value="2">Recovered cases</option>
+                            <option value="3">Active cases</option>
+                            <option value="4">Deaths</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="container" id="box2">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+
+            </form>
+
+        </div>
+
+    )
+
+};
