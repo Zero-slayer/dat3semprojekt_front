83dd9926771b77894bfada38ef374aeb3b158ec0
Buchandreas <43332738+Buchandreas@users.noreply.github.com>
2021-11-25 15:21:06 +0100

    Centered UI + implemented simple routes
    - Create routes
    - Fixed UI being offcenter

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -19,40 +19,40 @@ export default function FrontPage() {
         <div>
             <NavigationBar/>        
             <h1 id="header">Worldwide COVID-19 cases</h1>
-<div class="container">
-            <form className = "float-container">
-                <div className="float-child"> 
-                    <div className="container" id="box1">
-                        <div className="mb-3">
-                            <label htmlFor="countryTextInput" className="form-label">Country: </label>
-                            <select id="countryTextInput" className="form-select">
-                                <option defaultValue>Country</option>
-                                {renderCountries}
-                            </select>
+            <div class="container">
+                <form className = "float-container">
+                    <div className="float-child"> 
+                        <div className="container" id="box1">
+                            <div className="mb-3">
+                                <label htmlFor="countryTextInput" className="form-label">Country: </label>
+                                <select id="countryTextInput" className="form-select">
+                                    <option defaultValue>Country</option>
+                                    {renderCountries}
+                                </select>
+                            </div>
+                
+                            <div className="mb-3">
+                                <label htmlFor="filterTextInput" className="form-label">Filter:</label>
+                                <select id="filterTextInput" className="form-select">
+                                    <option defaultValue>Filter</option>
+                                    <option value="1">Confirmed cases</option>
+                                    <option value="2">Recovered cases</option>
+                                    <option value="3">Active cases</option>
+                                    <option value="4">Deaths</option>
+                                </select>
+                            </div>
                         </div>
-            
-                        <div className="mb-3">
-                            <label htmlFor="filterTextInput" className="form-label">Filter:</label>
-                            <select id="filterTextInput" className="form-select">
-                                <option defaultValue>Filter</option>
-                                <option value="1">Confirmed cases</option>
-                                <option value="2">Recovered cases</option>
-                                <option value="3">Active cases</option>
-                                <option value="4">Deaths</option>
-                            </select>
+                
+                        <div className="container" id="box2">
+                            <button type="submit" className="btn btn-primary">Submit</button>
                         </div>
                     </div>
-            
-                    <div className="container" id="box2">
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                
+                    <div className="float-child" id="leafletMap">
+                        <Map />
                     </div>
-                </div>
-            
-                <div className="float-child" id="leafletMap">
-                    <Map />
-                </div>
 
-            </form>
+                </form>
             </div>
         </div>
 
