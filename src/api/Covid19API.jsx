f47df76eb8a645c4c15b313cdb253a9c8f88d009
Mads Knudsen <35879356+Mfknudsen@users.noreply.github.com>
2021-11-30 16:40:48 +0100

    Sortet Country Names

--- src/api/Covid19API.jsx
+++ src/api/Covid19API.jsx
@@ -169,8 +169,18 @@ function Covid19API() {
             getData()
         }, [])
         return (
-            json
-        )
+            json.sort((a, b) => {
+                let fa = a["Country"].toLowerCase(),
+                    fb = b["Country"].toLowerCase();
+            
+                if (fa < fb) {
+                    return -1;
+                }
+                if (fa > fb) {
+                    return 1;
+                }
+                return 0;
+            })        )
     }
         return {
             Default,
